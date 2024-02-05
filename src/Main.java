02c934dc082c6516e18fea80762857409023009a
xizzin <144091701+xizzin@users.noreply.github.com>
2024-02-05 14:38:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 import java.time.LocalDate;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        
         System.out.print("Доступные действия программы: \n1. Приветствие\n2. Ввод возраста 3.Проверка возраста\n\n ДЛя выбора действия введите цифровое значение");
         Scanner in = new Scanner(System.in);
         System.out.print("Введите выбранное действие: ");
